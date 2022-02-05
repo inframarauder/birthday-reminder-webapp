393c7292419656dafad30e94ff959da90b5dbe25
subhasis020299 <subhasisdas125@gmail.com>
2022-02-05 18:44:19 +0530

    birthday record grid layout UI done:

---
+++ components/RecordCard.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { Edit, Delete } from "./Icons";
+
+const RecordCard = ({ item }) => {
+	return (
+		<div className="bg-gray-200 rounded-lg shadow-lg p-4 w-full px-5">
+			<p className="flex justify-between text-gray-700 text-lg">
+				<span>
+					{item.day} {item.month}
+				</span>
+				<span>
+					<button className="my-2 cursor-pointer hover:text-blue-500">
+						<Edit />
+					</button>
+					<button className="my-2 cursor-pointer hover:text-red-500">
+						<Delete />
+					</button>
+				</span>
+			</p>
+			<hr />
+			<p className="text-base">{item.name}</p>
+		</div>
+	);
+};
+
+export default RecordCard;
---
+++ pages/birthdays.js
@@ -0,0 +1,57 @@
+import React from "react";
+import Layout from "../components/Layout";
+import RecordCard from "../components/RecordCard";
+
+const data = [
+	{
+		name: "John Doe",
+		month: "January",
+		day: "1",
+	},
+	{
+		name: "Jane Doe",
+		month: "February",
+		day: "2",
+	},
+	{
+		name: "Jack Doe",
+		month: "March",
+		day: "3",
+	},
+	{
+		name: "Jill Doe",
+		month: "April",
+		day: "4",
+	},
+	{
+		name: "Joe Doe",
+		month: "May",
+		day: "5",
+	},
+	{
+		name: "Jenny Doe",
+		month: "June",
+		day: "6",
+	},
+];
+
+const Birthdays = () => {
+	return (
+		<Layout>
+			<div className="h-screen container my-20 px-5">
+				<h1 className="text-center text-lg font-bold text-gray-400">
+					{data.length} birthdays recorded
+				</h1>
+				<div className="my-4">
+					<div className="grid grid-cols-1 md:grid-cols-3 gap-10">
+						{data.map((item, index) => (
+							<RecordCard key={index} item={item} />
+						))}
+					</div>
+				</div>
+			</div>
+		</Layout>
+	);
+};
+
+export default Birthdays;
