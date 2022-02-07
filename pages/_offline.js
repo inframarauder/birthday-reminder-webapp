e9d102a601f66016898ba35a014ee3272a8f7223
subhasis020299 <subhasisdas125@gmail.com>
2022-02-07 20:42:47 +0530

    offline page configured

--- pages/_offline.js
+++ pages/_offline.js
@@ -1,12 +1,12 @@
 import React from "react";
 import { Offline } from "../components/Icons";
 
-const Offline = () => {
+const OfflinePage = () => {
 	return (
-		<div className="container h-screen">
-			<div className="mx-auto my-auto flex justify-center items-center">
+		<div className="container h-screen flex justify-center items-center">
+			<div className="flex justify-center items-center">
 				<Offline />
-				<span className="text-gray-500 text-center text-lg font-bold">
+				<span className="text-gray-500 text-center text-xl font-bold">
 					You are offline
 				</span>
 			</div>
@@ -14,4 +14,4 @@ const Offline = () => {
 	);
 };
 
-export default Offline;
+export default OfflinePage;
