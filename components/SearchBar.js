e2eab5a06bebaf30050c5830ba85ee6ec567d9c0
subhasis020299 <subhasisdas125@gmail.com>
2022-02-07 17:03:19 +0530

    UI Changes

--- components/SearchBar.js
+++ components/SearchBar.js
@@ -1,21 +1,18 @@
 import React from "react";
+import { Search } from "./Icons";
 
 const SearchBar = ({ searchBirthdaysByName }) => {
 	return (
 		<div className="flex justify-center">
-			<div className="mb-3 xl:w-96">
-				<div className="input-group relative flex flex-wrap items-stretch w-full mb-4">
-					<input
-						type="search"
-						className="form-control relative flex-auto min-w-0 block w-2/3 px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300  transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
-						placeholder="Search by friend's name..."
-						aria-label="Search"
-						aria-describedby="button-addon2"
-						onChange={(e) => {
-							searchBirthdaysByName(e.target.value);
-						}}
-					/>
-				</div>
+			<div class="pt-2 relative mx-auto text-gray-600">
+				<input
+					class="border-2 border-gray-300 bg-white h-10 px-5 pr-16 rounded-lg text-sm focus:outline-none focus:border-blue-400"
+					placeholder="Search by friend's name"
+					onChange={(e) => searchBirthdaysByName(e.target.value)}
+				/>
+				<span class="absolute right-0 top-0 mt-5 mr-4">
+					<Search />
+				</span>
 			</div>
 		</div>
 	);
